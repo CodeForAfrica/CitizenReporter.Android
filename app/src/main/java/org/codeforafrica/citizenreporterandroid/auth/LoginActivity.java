86a89728c7d85e3e3415e0753ce13635e6b77155
Phillip Ahereza <pahereza@gmail.com>
2017-07-25 14:14:24 +0300

    initial fb login

---
+++ app/src/main/java/org/codeforafrica/citizenreporterandroid/auth/LoginActivity.java
@@ -0,0 +1,140 @@
+package org.codeforafrica.citizenreporterandroid.auth;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.Profile;
+import com.facebook.ProfileTracker;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import org.codeforafrica.citizenreporterandroid.R;
+import org.codeforafrica.citizenreporterandroid.main.MainActivity;
+
+
+public class LoginActivity extends AppCompatActivity  {
+    private LoginButton loginButton;
+    private CallbackManager callbackManager;
+    private AccessTokenTracker mTokenTracker;
+    private ProfileTracker mProfileTracker;
+    private Profile profile;
+    private String first_name, last_name, fb_id;
+    private Uri profile_pic;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        callbackManager = CallbackManager.Factory.create();
+        setupTokenTracker();
+        setupProfileTracker();
+
+        mTokenTracker.startTracking();
+        mProfileTracker.startTracking();
+
+        setContentView(R.layout.activity_login);
+        loginButton = (LoginButton) findViewById(R.id.login_button);
+        final Intent intent = new Intent(this, MainActivity.class);
+
+        LoginManager.getInstance().registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.d("Facebook Login", "AccessToken: " + loginResult.getAccessToken().toString());
+                profile = Profile.getCurrentProfile();
+                first_name = profile.getFirstName();
+                last_name = profile.getLastName();
+                profile_pic = profile.getProfilePictureUri(400, 400);
+                fb_id = profile.getId();
+
+                Log.d("First name", first_name);
+                Log.d("Last name", last_name);
+                Log.d("FB_ID", fb_id);
+                Log.d("Profile_pic", profile_pic.toString());
+
+                startActivity(intent);
+
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
+//        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+//            @Override
+//            public void onSuccess(LoginResult loginResult) {
+//                Log.d("Facebook Login", "AccessToken: " + loginResult.getAccessToken().toString());
+//                profile = Profile.getCurrentProfile();
+//                first_name = profile.getFirstName();
+//                last_name = profile.getLastName();
+//                profile_pic = profile.getProfilePictureUri(400, 400);
+//                fb_id = profile.getId();
+//
+//                Log.d("First name", first_name);
+//                Log.d("Last name", last_name);
+//                Log.d("FB_ID", fb_id);
+//                Log.d("Profile_pic", profile_pic.toString());
+//
+//                startActivity(intent);
+//
+//            }
+//
+//            @Override
+//            public void onCancel() {
+//
+//            }
+//
+//            @Override
+//            public void onError(FacebookException error) {
+//
+//            }
+//        });
+
+    }
+
+    private void setupTokenTracker() {
+        mTokenTracker = new AccessTokenTracker() {
+            @Override
+            protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken, AccessToken currentAccessToken) {
+                Log.d("VIVZ", "" + currentAccessToken);
+            }
+        };
+    }
+
+    private void setupProfileTracker() {
+        mProfileTracker = new ProfileTracker() {
+            @Override
+            protected void onCurrentProfileChanged(Profile oldProfile, Profile currentProfile) {
+                Log.d("VIVZ", "" + currentProfile);
+            }
+        };
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+
+}
+
---
+++ app/src/main/java/org/codeforafrica/citizenreporterandroid/utils/APIHelper.java
@@ -0,0 +1,8 @@
+package org.codeforafrica.citizenreporterandroid.utils;
+
+/**
+ * Created by Ahereza on 7/25/17.
+ */
+
+public class APIHelper {
+}
