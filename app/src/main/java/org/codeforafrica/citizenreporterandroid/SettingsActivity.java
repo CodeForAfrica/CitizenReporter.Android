89aa724da047e8bc78af5f062b6c230fc3e82678
Jeffkungu <Jeffkungu@gmail.com>
2017-07-29 09:47:55 +0300

    [Chore] refactor code

--- app/src/main/java/org/codeforafrica/citizenreporterandroid/SettingsActivity.java
+++ app/src/main/java/org/codeforafrica/citizenreporterandroid/SettingsActivity.java
@@ -1,21 +1,27 @@
 package org.codeforafrica.citizenreporterandroid;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.SupportActivity;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class SettingsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
+        setContentView(R.layout.settings_list);
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -24,7 +30,47 @@ public class SettingsActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        ArrayList<String> menuItems = new ArrayList<>();
+        menuItems.add("About");
+        menuItems.add("Support Channel");
+        menuItems.add("Feedback");
+
+        ArrayAdapter<String> menuAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, menuItems);
+        ListView listView = (ListView) findViewById(R.id.list);
+        listView.setAdapter(menuAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                if (i == 0) {
+                    Intent aboutIntent = new Intent(getBaseContext(), AboutActivity.class);
+                    startActivity(aboutIntent);
+                } else if (i == 1) {
+                    Intent supportIntent = new Intent(getBaseContext(), SupportChannelActivity.class);
+                    startActivity(supportIntent);
+                } else {
+                    Intent feedbackIntent = new Intent(getBaseContext(), FeedbackActivity.class);
+                    startActivity(feedbackIntent);
+                }
+            }
+        });
     }
+
+    public void openAbout(View view) {
+        Intent about = new Intent(this, AboutActivity.class);
+        startActivity(about);
+    }
+
+    public void openSupportChannel(View view) {
+        Intent support = new Intent(this, SupportChannelActivity.class);
+        startActivity(support);
+    }
+
+    public void openFeedback(View view) {
+        Intent about = new Intent(this, FeedbackActivity.class);
+        startActivity(about);
+    }
+
 
 }
