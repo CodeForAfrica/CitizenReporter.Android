c4a533ef52c37bcd13defc90b31d715832b8f93c
Phillip Ahereza <pahereza@gmail.com>
2017-07-30 11:20:27 +0300

    added story model

---
+++ app/src/main/java/org/codeforafrica/citizenreporterandroid/models/Story.java
@@ -0,0 +1,88 @@
+package org.codeforafrica.citizenreporterandroid.models;
+
+import com.orm.SugarRecord;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by Ahereza on 7/30/17.
+ */
+
+public class Story extends SugarRecord<Story> {
+    private int assignmentId;
+    private String title;
+    private String why;
+    private String who;
+    private String author;
+    private String authorId;
+    private Date when;
+    private List<String> media;
+
+    public Story() {
+    }
+
+    public int getAssignmentId() {
+        return assignmentId;
+    }
+
+    public void setAssignmentId(int assignmentId) {
+        this.assignmentId = assignmentId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getWhy() {
+        return why;
+    }
+
+    public void setWhy(String why) {
+        this.why = why;
+    }
+
+    public String getWho() {
+        return who;
+    }
+
+    public void setWho(String who) {
+        this.who = who;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getAuthorId() {
+        return authorId;
+    }
+
+    public void setAuthorId(String authorId) {
+        this.authorId = authorId;
+    }
+
+    public Date getWhen() {
+        return when;
+    }
+
+    public void setWhen(Date when) {
+        this.when = when;
+    }
+
+    public List<String> getMedia() {
+        return media;
+    }
+
+    public void setMedia(List<String> media) {
+        this.media = media;
+    }
+}
