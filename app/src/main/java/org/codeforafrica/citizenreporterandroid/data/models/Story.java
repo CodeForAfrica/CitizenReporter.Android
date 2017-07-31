e0ccc89f9785b05bf364ad633a4d060acec9ad0f
Phillip Ahereza <pahereza@gmail.com>
2017-07-31 17:28:13 +0300

    added database helper class

--- app/src/main/java/org/codeforafrica/citizenreporterandroid/data/models/Story.java
+++ app/src/main/java/org/codeforafrica/citizenreporterandroid/data/models/Story.java
@@ -1,6 +1,8 @@
 package org.codeforafrica.citizenreporterandroid.data.models;
 
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 
@@ -9,15 +11,17 @@ import java.util.List;
  */
 
 public class Story {
+    private int local_id;
     private int assignmentId;
     private String title;
     private String why;
     private String who;
     private String author;
     private String authorId;
-    private Date when;
-    private List<String> media;
+    private String when;
+    private List<String> media = Collections.<String>emptyList();;
     private boolean uploaded;
+    private String updated;
 
     public Story() {
     }
@@ -70,21 +74,51 @@ public class Story {
         this.authorId = authorId;
     }
 
-    public Date getWhen() {
+    public String getWhen() {
         return when;
     }
 
-    public void setWhen(Date when) {
+    public void setWhen(String when) {
         this.when = when;
     }
 
-    public List<String> getMedia() {
-        return media;
+    public String getMedia() {
+        /**
+         * method returns a string consisting of all the paths the media attached separated by comas
+         * This is useful when saving the media to the database
+         */
+        if(media.size() > 0){
+            String concat_media = "";
+            for(String item: media){
+                concat_media = concat_media + item + ",";
+            }
+            return concat_media;
+        } else {
+            return "";
+        }
     }
 
-    public void setMedia(List<String> media) {
-        this.media = media;
+
+    public void addMedia(String media) {
+        /**
+         * adds a media item to the media list of the object
+         */
+        this.media.add(media);
     }
+
+    public List<String> readMediaFromDb(String media_csv) {
+        /**
+         * read a comma separated string from the database and convert it to a list
+         * First remove the trailing comma if it's there
+         */
+        char last_char = media_csv.charAt(media_csv.length() - 1);
+        if (last_char == ','){
+            media_csv = media_csv.substring(0, media_csv.length() - 2);
+        }
+        List<String> items = Arrays.asList(media_csv.split("\\s*,\\s*"));
+        return items;
+    }
+
 
     public boolean isUploaded() {
         return uploaded;
@@ -92,5 +126,21 @@ public class Story {
 
     public void setUploaded(boolean uploaded) {
         this.uploaded = uploaded;
+    }
+
+    public String getUpdated() {
+        return updated;
+    }
+
+    public void setUpdated(String updated) {
+        this.updated = updated;
+    }
+
+    public int getLocal_id() {
+        return local_id;
+    }
+
+    public void setLocal_id(int local_id) {
+        this.local_id = local_id;
     }
 }
---
+++ app/src/main/java/org/codeforafrica/citizenreporterandroid/utils/Constants.java
@@ -0,0 +1,28 @@
+package org.codeforafrica.citizenreporterandroid.utils;
+
+/**
+ * Created by Ahereza on 7/31/17.
+ */
+
+
+public class Constants {
+    public static final String DB_NAME = "citizenReporterDB";
+    public static final int DB_VERSION = 1;
+
+    // stories table
+    public static final String STORIES_TABLE_NAME = "storiesTBL";
+
+    // stories columns
+
+    public static final String KEY_ID = "id";
+    public static final String KEY_TITLE = "title";
+    public static final String KEY_ASSIGNMENT_ID = "assignment";
+    public static final String KEY_WHY = "why";
+    public static final String KEY_WHO = "who";
+    public static final String KEY_AUTHOR = "author";
+    public static final String KEY_AUTHOR_ID = "author_id";
+    public static final String KEY_WHEN = "when";
+    public static final String KEY_MEDIA = "media";
+    public static final String KEY_UPLOADED = "uploaded";
+    public static final String KEY_UPDATED = "updated";
+}
