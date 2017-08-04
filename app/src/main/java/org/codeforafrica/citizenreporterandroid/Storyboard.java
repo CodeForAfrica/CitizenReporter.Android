ecc0de69f0ff556bca6c6c0234a02d5444111203
Phillip Ahereza <pahereza@gmail.com>
2017-08-04 07:40:33 +0300

    added storyboard activity

---
+++ app/src/main/java/org/codeforafrica/citizenreporterandroid/Storyboard.java
@@ -0,0 +1,13 @@
+package org.codeforafrica.citizenreporterandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Storyboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_storyboard);
+    }
+}
