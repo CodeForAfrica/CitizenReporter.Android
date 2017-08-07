3e41b05f7cc40480a855254acf576d9ce1251de8
Jonathan Ngondi <jonathan.ngondi@andela.com>
2017-08-07 09:43:58 +0300

    ApiClient refactor

--- app/src/main/java/org/codeforafrica/citizenreporterandroid/main/api/ApiClient.java
+++ app/src/main/java/org/codeforafrica/citizenreporterandroid/main/api/ApiClient.java
@@ -8,18 +8,18 @@ import retrofit2.converter.gson.GsonConverterFactory;
  */
 
 public class ApiClient {
-    private static final String BASE_URL = "https://413cc8e4.ngrok.io";
+    private static final String BASE_URL = "https://0cc385e4.ngrok.io";
     private static Retrofit retrofit = null;
 
 
-    public static Retrofit getClient() {
-        if (retrofit==null){
-            retrofit = new Retrofit.Builder()
-                    .baseUrl(BASE_URL)
-                    .addConverterFactory(GsonConverterFactory.create())
-                    .build();
+    public static ApiInterface getApiClient() {
 
-        }
-        return retrofit;
+        retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        ApiInterface apiClient= retrofit.create(ApiInterface.class);
+
+        return apiClient;
     }
 }
