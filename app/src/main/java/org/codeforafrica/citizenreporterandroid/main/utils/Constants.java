a374af1408f92858d5d1588ea1a6c7a7d6ac26fe
Jonathan Ngondi <jonathan.ngondi@andela.com>
2017-08-07 09:48:13 +0300

    Create database

---
+++ app/src/main/java/org/codeforafrica/citizenreporterandroid/main/utils/Constants.java
@@ -0,0 +1,27 @@
+package org.codeforafrica.citizenreporterandroid.main.utils;
+
+/**
+ * Created by Mugiwara_Munyi on 05/08/2017.
+ */
+
+public class Constants {
+
+    public static final String DB_NAME = "citizenReporterDB";
+    public static final int DB_VERSION = 1;
+
+    //Assignments table
+    public static final String ASSIGNMENTS_TABLE_NAME = "assignmentsTBL";
+
+    //Assignments columns
+
+    public static final String KEY_ID = "id";
+    public static final String KEY_ASSIGNMENT_TITLE = "title";
+    public static final String KEY_ASSIGNMENT_DESCRIPTION = "description";
+    public static final String KEY_ASSIGNMENT_MEDIA = "requiredMedia";
+    public static final String KEY_ASSIGNMENT_RESPONSES = "numberOfResponses";
+    public static final String KEY_ASSIGNMENT_AUTHOR = "author";
+    public static final String KEY_ASSIGNMENT_DEADLINE = "deadline";
+    public static final String KEY_ASSIGNMENT_LOCATION = "assignmentLocation";
+
+    public static final String KEY_ASSIGNMENT_UPDATED = "updated";
+}
