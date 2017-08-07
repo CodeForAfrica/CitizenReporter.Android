353d34bda38475b1baf3d0783891f28e545aa41e
Jonathan Ngondi <jonathan.ngondi@andela.com>
2017-08-07 07:36:50 +0300

    Refactor of the MainActivity

--- app/src/main/java/org/codeforafrica/citizenreporterandroid/main/MainActivity.java
+++ app/src/main/java/org/codeforafrica/citizenreporterandroid/main/MainActivity.java
@@ -1,177 +1,80 @@
 package org.codeforafrica.citizenreporterandroid.main;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.DefaultItemAnimator;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 
 import org.codeforafrica.citizenreporterandroid.R;
-import org.codeforafrica.citizenreporterandroid.main.adapter.AssignmentsAdapter;
 import org.codeforafrica.citizenreporterandroid.main.api.ApiClient;
 import org.codeforafrica.citizenreporterandroid.main.api.ApiInterface;
 import org.codeforafrica.citizenreporterandroid.main.models.Assignments;
-import org.codeforafrica.citizenreporterandroid.main.models.AssignmentsResponse;
 
-import java.util.ArrayList;
-import java.util.List;
 
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 
 public class MainActivity extends AppCompatActivity {
-    private AssignmentsAdapter adapter;
-    private ArrayList<Assignments> assignmentsList;
-    /**
-     * The {@link android.support.v4.view.PagerAdapter} that will provide
-     * fragments for each of the sections. We use a
-     * {@link FragmentPagerAdapter} derivative, which will keep every
-     * loaded fragment in memory. If this becomes too memory intensive, it
-     * may be best to switch to a
-     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
-     */
-    private SectionsPagerAdapter mSectionsPagerAdapter;
 
-    /**
-     * The {@link ViewPager} that will host the section contents.
-     */
-    private ViewPager mViewPager;
+    private SharedPreferences preferences;
+    private ApiInterface apiClient;
+
+    @BindView(R.id.navigation)
+    BottomNavigationView bottomNavigationView;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        apiClient = ApiClient.getApiClient();
+        preferences = PreferenceManager.getDefaultSharedPreferences(MainActivity.this);
 
-        Log.d("MainActivity", "onCreate: ");
+        bottomNavigationView.setOnNavigationItemSelectedListener
+                (new BottomNavigationView.OnNavigationItemSelectedListener(){
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        Fragment selectedFragment = null;
+                        switch (item.getItemId()){
+                            case R.id.assignments_item:
+                                selectedFragment = AssignmentsFragment.newInstance();
+                                break;
+                            case R.id.stories_item:
+                                selectedFragment = StoriesFragment.newInstance();
+                                break;
+                            case R.id.about_item:
+                                selectedFragment = AboutFragment.newInstance();
+                                break;
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        // Create the adapter that will return a fragment for each of the three
-        // primary sections of the activity.
-        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+                        }
+                        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                        transaction.replace(R.id.frame_layout, selectedFragment);
+                        transaction.commit();
+                        return true;
+                    }
 
-        // Set up the ViewPager with the sections adapter.
-        mViewPager = (ViewPager) findViewById(R.id.container);
-        mViewPager.setAdapter(mSectionsPagerAdapter);
-
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
-        tabLayout.setupWithViewPager(mViewPager);
-
-
-        initData();
+                });
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_layout, AssignmentsFragment.newInstance());
+        transaction.commit();
 
     }
 
-
-    private void initData(){
-
-        ApiInterface apiService = ApiClient.getClient().create(ApiInterface.class);
-        Call<List<Assignments>> call = apiService.getAssignments();
-
-        call.enqueue(new Callback<List<Assignments>>(){
-            @Override
-            public void onResponse(Call<List<Assignments>> call, Response<List<Assignments>> response){
-                int ResponseCode = response.code();
-                List<Assignments> responseBody =  response.body();
-                System.out.println(response.code());
-                System.out.println(response.body());
-                assignmentsList = new ArrayList<Assignments>();
-
-                if (!responseBody.isEmpty()){
-                    for (Assignments res: responseBody){
-                           assignmentsList.add(res);
-                }  };
-
-            }
-            @Override
-            public void onFailure(Call call, Throwable t) {
-
-                Log.e("Something's Wrong", t.toString());
-
-            }
-
-        });
-    }
-
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    /**
-     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
-     * one of the sections/tabs/pages.
-     */
-    public class SectionsPagerAdapter extends FragmentPagerAdapter {
-
-        public SectionsPagerAdapter(FragmentManager fm) {
-            super(fm);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            // getItem is called to instantiate the fragment for the given page.
-            // Return a PlaceholderFragment (defined as a static inner class below).
-            switch (position){
-                case 0:
-                    return AssignmentsFragment.newInstance(assignmentsList);
-                case 1:
-                    return StoriesFragment.newInstance();
-                case 2:
-                    return AboutFragment.newInstance();
-                default:
-                    return null;
-            }
-        }
-
-        @Override
-        public int getCount() {
-            // Show 3 total pages.
-            return 3;
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            switch (position) {
-                case 0:
-                    return "Assignments";
-                case 1:
-                    return "Stories";
-                case 2:
-                    return "About";
-            }
-            return null;
-        }
-    }
 }
