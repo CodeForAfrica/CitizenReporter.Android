584841b5a46e201110539c03176c69e90d6b4a44
Phillip Ahereza <pahereza@gmail.com>
2017-08-07 14:05:09 +0300

    search and find location google

--- app/src/main/java/org/codeforafrica/citizenreporterandroid/Storyboard.java
+++ app/src/main/java/org/codeforafrica/citizenreporterandroid/Storyboard.java
@@ -3,23 +3,29 @@ package org.codeforafrica.citizenreporterandroid;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.daimajia.slider.library.SliderLayout;
 import com.daimajia.slider.library.SliderTypes.BaseSliderView;
 import com.daimajia.slider.library.SliderTypes.TextSliderView;
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlaceAutocomplete;
 
 import org.codeforafrica.citizenreporterandroid.data.models.Story;
 import org.codeforafrica.citizenreporterandroid.data.sources.LocalDataHelper;
 import org.codeforafrica.citizenreporterandroid.main.MainActivity;
-import org.codeforafrica.citizenreporterandroid.main.stories.StoriesFragment;
 import org.codeforafrica.citizenreporterandroid.utils.Constants;
 
 import java.util.HashMap;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class Storyboard extends AppCompatActivity {
     @BindView(R.id.slider) SliderLayout storiesSlider;
@@ -27,8 +33,10 @@ public class Storyboard extends AppCompatActivity {
     @BindView(R.id.cause) TextView whatCausedthis;
     @BindView(R.id.who_is_involved) TextView whoIsInvolved;
     @BindView(R.id.when_happened) TextView whenDidItHappen;
+    @BindView(R.id.location) TextView location;
     private Story story;
     private LocalDataHelper dataHelper;
+    private int PLACE_AUTOCOMPLETE_REQUEST_CODE = 31;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -104,4 +112,36 @@ public class Storyboard extends AppCompatActivity {
 //
 //    @OnClick(R.id.caused_view)
 //    private static void openWhatCausedThis() {}
+
+    @OnClick(R.id.where_view)
+    public void getLocation() {
+        try {
+            Intent intent =
+                    new PlaceAutocomplete.IntentBuilder(PlaceAutocomplete.MODE_FULLSCREEN)
+                            .build(this);
+            startActivityForResult(intent, PLACE_AUTOCOMPLETE_REQUEST_CODE);
+        } catch (GooglePlayServicesRepairableException e) {
+            // TODO: Handle the error.
+        } catch (GooglePlayServicesNotAvailableException e) {
+            // TODO: Handle the error.
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == PLACE_AUTOCOMPLETE_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                Place place = PlaceAutocomplete.getPlace(this, data);
+                location.setText(place.getName());
+                Log.i("Storybaord", "Place: " + place.getName());
+            } else if (resultCode == PlaceAutocomplete.RESULT_ERROR) {
+                Status status = PlaceAutocomplete.getStatus(this, data);
+                // TODO: Handle the error.
+                Log.i("Storybaord", status.getStatusMessage());
+
+            } else if (resultCode == RESULT_CANCELED) {
+                // The user canceled the operation.
+            }
+        }
+    }
 }
