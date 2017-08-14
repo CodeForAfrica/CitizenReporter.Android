bd0a587493aca4f44a9526aa2243341753dc467f
Jonathan Ngondi <jonathan.ngondi@andela.com>
2017-08-14 16:59:05 +0300

    Fix for ButterKnife

--- app/src/main/java/org/codeforafrica/citizenreporterandroid/main/adapter/AssignmentsAdapter.java
+++ app/src/main/java/org/codeforafrica/citizenreporterandroid/main/adapter/AssignmentsAdapter.java
@@ -7,11 +7,13 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
 
+import org.codeforafrica.citizenreporterandroid.main.MainActivity;
 import org.codeforafrica.citizenreporterandroid.main.models.Assignments;
 import org.codeforafrica.citizenreporterandroid.R;
 
@@ -26,50 +28,18 @@ import butterknife.ButterKnife;
 
 public class AssignmentsAdapter extends
         RecyclerView.Adapter<AssignmentsAdapter.AssignmentsViewHolder> {
-    private Context context;
-    private List<Assignments> assignmentsList;
-
-
-    public void setAssignmentList(List<Assignments> assignmentsList) {
-        this.assignmentsList = assignmentsList;
-    }
-
-    public AssignmentsAdapter(Context mContext, List<Assignments> assignmentsList) {
-        this.context = context;
-        this.assignmentsList = assignmentsList;
-
-    }
-
-    @Override
-    public AssignmentsViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.fragment_assignments, parent, false);
-        AssignmentsViewHolder viewHolder = new AssignmentsViewHolder(view);
-
-        return viewHolder;
-    }
-
-    @Override
-    public void onBindViewHolder(AssignmentsViewHolder viewHolder, int position) {
-
-        Assignments assignment = assignmentsList.get(position);
-
-        viewHolder.assignment_title.setText(assignment.getTitle());
-
-        viewHolder.assignment_deadline.setText(assignment.getDeadline());
-
-    }
-
-    @Override
-    public int getItemCount() {
-        return assignmentsList.size();
-    }
 
     public class AssignmentsViewHolder extends RecyclerView.ViewHolder {
         @BindView(R.id.assignment_title)
         TextView assignment_title;
         @BindView(R.id.assignment_deadline)
         TextView assignment_deadline;
-
+        @BindView(R.id.assignment_location)
+        TextView assignment_location;
+        @BindView(R.id.assignment_author)
+        TextView assignment_author;
+        @BindView(R.id.assignment_details)
+        Button assignment_details;
 
         public AssignmentsViewHolder(View view) {
             super(view);
@@ -79,4 +49,52 @@ public class AssignmentsAdapter extends
 
 
     }
+
+    private Context mContext;
+    private List<Assignments> assignmentsList;
+
+
+    public void setAssignmentList(List<Assignments> assignmentsList) {
+        this.assignmentsList = assignmentsList;
+    }
+
+    public AssignmentsAdapter(List<Assignments> assignmentsList, Context context) {
+        mContext = context;
+        this.assignmentsList = assignmentsList;
+    }
+
+
+    @Override
+    public AssignmentsViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.fragment_assignments, parent, false);
+
+
+        return new AssignmentsViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(final AssignmentsViewHolder viewHolder, int position) {
+
+        Assignments assignment = assignmentsList.get(position);
+
+        viewHolder.assignment_title.setText(assignment.getTitle());
+        viewHolder.assignment_deadline.setText(assignment.getDeadline());
+        viewHolder.assignment_location.setText(assignment.getAssignmentLocation());
+        viewHolder.assignment_author.setText(assignment.getAuthor());
+        viewHolder.assignment_details.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(mContext, MainActivity.class);
+                mContext.startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return assignmentsList.size();
+    }
+
+
 }
