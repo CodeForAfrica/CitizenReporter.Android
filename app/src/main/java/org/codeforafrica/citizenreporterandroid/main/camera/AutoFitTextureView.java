e7d4d8987396f1da4a2440a9b803050ce77bdb3f
Jonathan Ngondi <jonathan.ngondi@andela.com>
2017-08-19 13:19:59 +0300

    Added functionality for camera using camera2 API -Changed minSDK from 19 to 21 -Added AutofitTextureView for the preview class of the camera -Added core camera functionality to OverlayCameraFragment though incomplete -Main logic for OverlayCameraActivity finished

---
+++ app/src/main/java/org/codeforafrica/citizenreporterandroid/main/camera/AutoFitTextureView.java
@@ -0,0 +1,8 @@
+package org.codeforafrica.citizenreporterandroid.main.camera;
+
+/**
+ * Created by Mugiwara_Munyi on 18/08/2017.
+ */
+
+class AutoFitTextureView {
+}
---
+++ app/src/main/java/org/codeforafrica/citizenreporterandroid/main/camera/OverlayCameraFragment.java
@@ -0,0 +1,8 @@
+package org.codeforafrica.citizenreporterandroid.main.camera;
+
+/**
+ * Created by Mugiwara_Munyi on 17/08/2017.
+ */
+
+public class OverlayCameraFragment {
+}
