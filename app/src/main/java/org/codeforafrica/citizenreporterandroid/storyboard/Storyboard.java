c7487b8b25d7f5f784920cdf643c0e26b00ef761
Phillip Ahereza <pahereza@gmail.com>
2017-08-24 09:25:52 +0300

    added margins between the attachment tiles

--- app/src/main/java/org/codeforafrica/citizenreporterandroid/storyboard/Storyboard.java
+++ app/src/main/java/org/codeforafrica/citizenreporterandroid/storyboard/Storyboard.java
@@ -13,12 +13,14 @@ import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.view.menu.MenuBuilder;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.PopupMenu;
 import android.support.v7.widget.RecyclerView;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
@@ -27,6 +29,7 @@ import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -41,6 +44,7 @@ import com.google.android.gms.common.GooglePlayServicesRepairableException;
 import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.places.Place;
 import com.google.android.gms.location.places.ui.PlaceAutocomplete;
+import com.squareup.picasso.Picasso;
 
 import org.codeforafrica.citizenreporterandroid.R;
 import org.codeforafrica.citizenreporterandroid.data.models.Story;
@@ -50,6 +54,7 @@ import org.codeforafrica.citizenreporterandroid.utils.Constants;
 import org.codeforafrica.citizenreporterandroid.utils.MediaUtils;
 import org.codeforafrica.citizenreporterandroid.utils.StoryBoardUtils;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.HashMap;
@@ -70,9 +75,6 @@ public class Storyboard extends AppCompatActivity implements DatePickerDialog.On
     @BindView(R.id.storybaord_date)
     Button date;
 
-    @BindView(R.id.attachments_recyclerview)
-    RecyclerView attachmentsRecyclerview;
-
     @BindView(R.id.story_title)
     EditText story_title;
 
@@ -85,18 +87,20 @@ public class Storyboard extends AppCompatActivity implements DatePickerDialog.On
     @BindView(R.id.attachments_button)
     ImageView attachmentsMenuBtn;
 
+    @BindView(R.id.attachmentsLayout)
+    LinearLayout attachmentsLayout;
+
 
     private LocalDataHelper dataHelper;
     private PopupMenu popupMenu;
     private Story activeStory;
     private SharedPreferences preferences;
     private final Calendar calendar = Calendar.getInstance();
-    private AttachmentsAdapter attachmentsAdapter;
-    private List<String> attachmentsList = new ArrayList<>();
 
     private final DatePickerDialog datePickerDialog = DatePickerDialog.newInstance(
             this, calendar.get(Calendar.YEAR),
             calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH));
+    private LayoutInflater inflater;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -104,9 +108,9 @@ public class Storyboard extends AppCompatActivity implements DatePickerDialog.On
         setContentView(R.layout.activity_storyboard);
         dataHelper = new LocalDataHelper(this);
         ButterKnife.bind(this);
-        attachmentsRecyclerview.setHasFixedSize(true);
-        attachmentsRecyclerview.setLayoutManager(new LinearLayoutManager(this));
         String action = getIntent().getAction();
+        inflater = LayoutInflater.from(Storyboard.this);
+
 
         if (action.equals(Constants.ACTION_EDIT_VIEW_STORY)) {
             long storyID = getIntent().getLongExtra("STORY_ID", -1);
@@ -157,13 +161,11 @@ public class Storyboard extends AppCompatActivity implements DatePickerDialog.On
         popupMenu.inflate(R.menu.attachments_menu);
 
 
-        attachmentsAdapter = new AttachmentsAdapter(attachmentsList, this);
-        attachmentsRecyclerview.setAdapter(attachmentsAdapter);
-        attachmentsRecyclerview.setNestedScrollingEnabled(false);
-
         StoryBoardUtils.requestPermission(this, Manifest.permission.RECORD_AUDIO);
         StoryBoardUtils.requestPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
 
+        addAudioAttachment();
+        addVideoAttachment();
 
     }
 
@@ -287,10 +289,7 @@ public class Storyboard extends AppCompatActivity implements DatePickerDialog.On
                         for (Uri uri : uriList) {
                             Log.d("IMAGE SELECTOR", "onImagesSelected: "
                                     + MediaUtils.getPathFromUri(context, uri));
-                            attachmentsList.add(MediaUtils.getPathFromUri(context, uri));
                         }
-                        Log.d("IMAGE PICKER", "openImagePicker: " + attachmentsList.size());
-                        attachmentsAdapter.updateList(attachmentsList);
 
                     }
                 })
@@ -303,6 +302,50 @@ public class Storyboard extends AppCompatActivity implements DatePickerDialog.On
 
         bottomSheetDialogFragment.show(getSupportFragmentManager());
     }
+
+    public void addImageAttachment(Uri uri) {
+        View view = inflater.inflate(R.layout.item_image, null);
+        TextView filename = (TextView) view.findViewById(R.id.image_filename_tv);
+        TextView filesize = (TextView) view.findViewById(R.id.image_filesize_tv);
+        ImageView image = (ImageView) view.findViewById(R.id.attached_image);
+
+        filename.setText("IMG_11203.jpg");
+        filesize.setText("1.5MB");
+
+        Picasso.with(this)
+                .load(uri)
+                .into(image);
+
+
+
+        attachmentsLayout.addView(view);
+    }
+
+    public void addAudioAttachment() {
+        View view = inflater.inflate(R.layout.item_audio, null);
+        TextView filename = (TextView) view.findViewById(R.id.audio_filename_tv);
+        TextView filesize = (TextView) view.findViewById(R.id.audio_filesize_tv);
+
+        filename.setText("recording.mp3");
+        filesize.setText("1.5MB");
+
+        attachmentsLayout.addView(view);
+
+    }
+
+    public void addVideoAttachment() {
+        View view = inflater.inflate(R.layout.item_video, null);
+        TextView filename = (TextView) view.findViewById(R.id.video_filename_tv);
+        TextView filesize = (TextView) view.findViewById(R.id.video_filesize_tv);
+
+
+        filename.setText("recording.mp4");
+        filesize.setText("1.5MB");
+
+        attachmentsLayout.addView(view);
+
+    }
+
 
 
 }
