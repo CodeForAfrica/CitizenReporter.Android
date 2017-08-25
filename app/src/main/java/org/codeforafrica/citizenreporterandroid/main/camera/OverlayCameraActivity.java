b5ab914c84e4c18330b8f4a663d064fe0f38df16
Jonathan Ngondi <jonathan.ngondi@andela.com>
2017-08-25 06:34:25 +0300

    Logic for passing Extras from Storyboard to OverlayCameraFragment

--- app/src/main/java/org/codeforafrica/citizenreporterandroid/main/camera/OverlayCameraActivity.java
+++ app/src/main/java/org/codeforafrica/citizenreporterandroid/main/camera/OverlayCameraActivity.java
@@ -1,6 +1,7 @@
 package org.codeforafrica.citizenreporterandroid.main.camera;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 import org.codeforafrica.citizenreporterandroid.R;
@@ -11,14 +12,20 @@ import org.codeforafrica.citizenreporterandroid.R;
 
 public class OverlayCameraActivity extends Activity {
 
+    Intent intent;
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
+        intent = getIntent();
+        int g = intent.getIntExtra("group", 0);
+
         setContentView(R.layout.activity_camera);
         if (null == savedInstanceState){
             getFragmentManager().beginTransaction()
-                    .replace(R.id.container, OverlayCameraFragment.newInstance())
+                    .replace(R.id.container, OverlayCameraFragment.newInstance(g))
                     .commit();
+
         }
     }
 
