1f8680e612de597c5a6e76cbfb66fe5c65456f04
Jeffkungu <Jeffkungu@gmail.com>
2017-09-08 10:45:04 +0300

    [Feature] adds methods to annimate the deleting process by drawing a red background and an x-mark after a story is deleted

--- app/src/main/java/org/codeforafrica/citizenreporterandroid/main/stories/StoriesFragment.java
+++ app/src/main/java/org/codeforafrica/citizenreporterandroid/main/stories/StoriesFragment.java
@@ -1,19 +1,24 @@
 package org.codeforafrica.citizenreporterandroid.main.stories;
 
 import android.content.SharedPreferences;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import butterknife.BindView;
-import butterknife.ButterKnife;
-import java.util.List;
+
 import org.codeforafrica.citizenreporterandroid.R;
 import org.codeforafrica.citizenreporterandroid.adapter.StoriesRecyclerViewAdapter;
 import org.codeforafrica.citizenreporterandroid.data.models.Story;
@@ -22,65 +27,219 @@ import org.codeforafrica.citizenreporterandroid.utils.APIClient;
 import org.codeforafrica.citizenreporterandroid.utils.CReporterAPI;
 import org.codeforafrica.citizenreporterandroid.utils.NetworkHelper;
 
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class StoriesFragment extends Fragment {
-  @BindView(R.id.stories_recyclerview) RecyclerView storiesRecyclerView;
-  private List<Story> stories;
-  private StoriesRecyclerViewAdapter adapter;
-  private LocalDataHelper dataHelper;
-  private SharedPreferences preferences;
-  private CReporterAPI apiClient;
+    @BindView(R.id.stories_recyclerview)
+    RecyclerView storiesRecyclerView;
+    private List<Story> stories;
+    private StoriesRecyclerViewAdapter adapter;
+    private LocalDataHelper dataHelper;
+    private SharedPreferences preferences;
+    private CReporterAPI apiClient;
 
-  public StoriesFragment() {
-    // Required empty public constructor
-  }
-
-  // TODO: Rename and change types and number of parameters
-  public static StoriesFragment newInstance() {
-    StoriesFragment fragment = new StoriesFragment();
-    return fragment;
-  }
-
-  @Override public void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-  }
-
-  @Override public View onCreateView(LayoutInflater inflater, ViewGroup container,
-      Bundle savedInstanceState) {
-    // Inflate the layout for this fragment
-    View view = inflater.inflate(R.layout.fragment_stories, container, false);
-    ButterKnife.bind(this, view);
-    return view;
-  }
-
-  @Override public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
-    super.onViewCreated(view, savedInstanceState);
-    preferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
-    storiesRecyclerView.setHasFixedSize(true);
-    storiesRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
-
-    dataHelper = new LocalDataHelper(getActivity());
-
-    stories = dataHelper.getAllStories();
-
-    adapter = new StoriesRecyclerViewAdapter(stories, getContext());
-
-    storiesRecyclerView.setAdapter(adapter);
-
-    if (dataHelper.getStoriesCount() == 0) {
-      apiClient = APIClient.getApiClient();
-      String fb_id = preferences.getString("fb_id", "");
-      if (fb_id != "") {
-        Log.d("API", "making network call get stories");
-        NetworkHelper.getUserStories(getActivity(), apiClient, fb_id, adapter);
-      }
+    public StoriesFragment() {
+        // Required empty public constructor
     }
 
-    //        adapter.setStoryList(dataHelper.getAllStories());
-    //        adapter.notifyDataSetChanged();
-  }
+    // TODO: Rename and change types and number of parameters
+    public static StoriesFragment newInstance() {
+        StoriesFragment fragment = new StoriesFragment();
+        return fragment;
+    }
 
-  @Override public void onResume() {
-    adapter.notify(dataHelper.getAllStories());
-    super.onResume();
-  }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_stories, container, false);
+        ButterKnife.bind(this, view);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        preferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        storiesRecyclerView.setHasFixedSize(true);
+        storiesRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        dataHelper = new LocalDataHelper(getActivity());
+
+        stories = dataHelper.getAllStories();
+
+        adapter = new StoriesRecyclerViewAdapter(stories, getContext());
+
+        storiesRecyclerView.setAdapter(adapter);
+
+        if (dataHelper.getStoriesCount() == 0) {
+            apiClient = APIClient.getApiClient();
+            String fb_id = preferences.getString("fb_id", "");
+            if (fb_id != "") {
+                Log.d("API", "making network call get stories");
+                NetworkHelper.getUserStories(getActivity(), apiClient, fb_id, adapter);
+            }
+
+            setUpItemTouchHelper();
+            setUpAnimationDecoratorHelper();
+        }
+
+        //        adapter.setStoryList(dataHelper.getAllStories());
+        //        adapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void onResume() {
+        adapter.notify(dataHelper.getAllStories());
+        super.onResume();
+    }
+
+    private void setUpItemTouchHelper() {
+
+        ItemTouchHelper.SimpleCallback simpleItemTouchCallback = new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
+
+            Drawable background;
+            Drawable xMark;
+            int xMarkMargin;
+            boolean initiated;
+
+            private void init() {
+                background = new ColorDrawable(Color.RED);
+                xMark = ContextCompat.getDrawable(getContext(), R.drawable.ic_clear_black_24dp);
+                xMark.setColorFilter(Color.WHITE, PorterDuff.Mode.SRC_ATOP);
+                xMarkMargin = (int) getContext().getResources().getDimension(R.dimen.ic_clear_margin);
+                initiated = true;
+            }
+
+            @Override
+            public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
+                return false;
+            }
+
+            @Override
+            public int getSwipeDirs(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder) {
+                int position = viewHolder.getAdapterPosition();
+                StoriesRecyclerViewAdapter storiesRecyclerViewAdapter = (StoriesRecyclerViewAdapter) recyclerView.getAdapter();
+                if (storiesRecyclerViewAdapter.isUndoOn() && storiesRecyclerViewAdapter.isPendingRemoval(position)) {
+                    return 0;
+                }
+                return super.getSwipeDirs(recyclerView, viewHolder);
+            }
+
+            @Override
+            public void onSwiped(RecyclerView.ViewHolder viewHolder, int swipeDir) {
+                int swipedPosition = viewHolder.getAdapterPosition();
+                StoriesRecyclerViewAdapter adapter = (StoriesRecyclerViewAdapter) storiesRecyclerView.getAdapter();
+                boolean undoOn = adapter.isUndoOn();
+                if (undoOn) {
+                    adapter.pendingRemoval(swipedPosition);
+                } else {
+                    adapter.remove(swipedPosition);
+                }
+            }
+
+            @Override
+            public void onChildDraw(Canvas c, RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
+                View itemView = viewHolder.itemView;
+
+                if (viewHolder.getAdapterPosition() == -1) {
+                    return;
+                }
+
+                if (!initiated) {
+                    init();
+                }
+
+                // draw red background
+                background.setBounds(itemView.getRight() + (int) dX, itemView.getTop(), itemView.getRight(), itemView.getBottom());
+                background.draw(c);
+
+                // draw x mark
+                int itemHeight = itemView.getBottom() - itemView.getTop();
+                int intrinsicWidth = xMark.getIntrinsicWidth();
+                int intrinsicHeight = xMark.getIntrinsicWidth();
+
+                int xMarkLeft = itemView.getRight() - xMarkMargin - intrinsicWidth;
+                int xMarkRight = itemView.getRight() - xMarkMargin;
+                int xMarkTop = itemView.getTop() + (itemHeight - intrinsicHeight) / 2;
+                int xMarkBottom = xMarkTop + intrinsicHeight;
+                xMark.setBounds(xMarkLeft, xMarkTop, xMarkRight, xMarkBottom);
+
+                xMark.draw(c);
+
+                super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+            }
+
+        };
+        ItemTouchHelper mItemTouchHelper = new ItemTouchHelper(simpleItemTouchCallback);
+        mItemTouchHelper.attachToRecyclerView(storiesRecyclerView);
+    }
+
+    private void setUpAnimationDecoratorHelper() {
+        storiesRecyclerView.addItemDecoration(new RecyclerView.ItemDecoration() {
+
+            Drawable background;
+            boolean initiated;
+
+            private void init() {
+                background = new ColorDrawable(Color.RED);
+                initiated = true;
+            }
+
+            @Override
+            public void onDraw(Canvas c, RecyclerView parent, RecyclerView.State state) {
+
+                if (!initiated) {
+                    init();
+                }
+
+                if (parent.getItemAnimator().isRunning()) {
+                    View lastViewComingDown = null;
+                    View firstViewComingUp = null;
+
+                    int left = 0;
+                    int right = parent.getWidth();
+                    int top = 0;
+                    int bottom = 0;
+                    int childCount = parent.getLayoutManager().getChildCount();
+                    for (int i = 0; i < childCount; i++) {
+                        View child = parent.getLayoutManager().getChildAt(i);
+                        if (child.getTranslationY() < 0) {
+                            lastViewComingDown = child;
+                        } else if (child.getTranslationY() > 0) {
+                            if (firstViewComingUp == null) {
+                                firstViewComingUp = child;
+                            }
+                        }
+                    }
+
+                    if (lastViewComingDown != null && firstViewComingUp != null) {
+                        top = lastViewComingDown.getBottom() + (int) lastViewComingDown.getTranslationY();
+                        bottom = firstViewComingUp.getTop() + (int) firstViewComingUp.getTranslationY();
+                    } else if (lastViewComingDown != null) {
+                        top = lastViewComingDown.getBottom() + (int) lastViewComingDown.getTranslationY();
+                        bottom = lastViewComingDown.getBottom();
+                    } else if (firstViewComingUp != null) {
+                        top = firstViewComingUp.getTop();
+                        bottom = firstViewComingUp.getTop() + (int) firstViewComingUp.getTranslationY();
+                    }
+
+                    background.setBounds(left, top, right, bottom);
+                    background.draw(c);
+
+                }
+                super.onDraw(c, parent, state);
+            }
+
+        });
+    }
+
 }
