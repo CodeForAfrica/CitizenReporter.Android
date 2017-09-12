efc3328953d6e2b78fb8abae93d31db124e4cb91
Jeffkungu <Jeffkungu@gmail.com>
2017-09-12 10:52:23 +0300

    [Feature] refactor code for custom push notification display

--- app/src/main/java/org/codeforafrica/citizenreporterandroid/main/assignments/MyFirebaseMessagingService.java
+++ app/src/main/java/org/codeforafrica/citizenreporterandroid/main/assignments/MyFirebaseMessagingService.java
@@ -1,15 +1,21 @@
 package org.codeforafrica.citizenreporterandroid.main.assignments;
 
+import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
-import android.support.v4.app.NotificationCompat;
+import android.graphics.Color;
+import android.graphics.drawable.Icon;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.util.Log;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
 import org.codeforafrica.citizenreporterandroid.R;
+import org.json.JSONObject;
 
 /**
  * Created by Jeffkungu on 08/09/2017.
@@ -22,18 +28,59 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
+
+        if (remoteMessage.getData().size() > 0) {
+            try {
+
+                JSONObject jsonObject = new JSONObject(remoteMessage.getData());
+                Log.e("Tag", remoteMessage.getData().toString());
+
+
+                sendNotification(remoteMessage.getData().toString());
+
+
+            } catch (Exception e) {
+
+            }
+
+        }
+
+    }
+
+    private void sendNotification(String message) {
+
         Intent intent = new Intent(this, AssignmentDetailActivity.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this,0,intent,PendingIntent.FLAG_ONE_SHOT);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT);
 
-        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this);
-        notificationBuilder.setContentTitle("New Assignments");
-        notificationBuilder.setContentText(remoteMessage.getNotification().getBody());
-        notificationBuilder.setAutoCancel(true);
-        notificationBuilder.setSmallIcon(R.mipmap.ic_launcher);
-        notificationBuilder.setContentIntent(pendingIntent);
+        long time = System.currentTimeMillis();
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        Notification.Builder notification = new Notification.Builder(this); 
+
+        if(android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M){
+            Notification.Action action = new Notification.Action.Builder(Icon.createWithResource(this, R.mipmap.ic_archive), "View", pendingIntent).build();
+            notification.setAutoCancel(true);
+            notification.setSmallIcon(R.mipmap.ic_launcher);
+            notification.setSound(defaultSoundUri);
+            notification.setContentTitle("New Assignments");
+            notification.setContentText(message);
+            notification.setWhen(time);
+            notification.setColor(Color.TRANSPARENT);
+            notification.setContentIntent(pendingIntent);
+            notification.addAction(action);
+        } else {
+            notification.setAutoCancel(true);
+            notification.setSmallIcon(R.mipmap.ic_launcher);
+            notification.setSound(defaultSoundUri);
+            notification.setContentTitle("New Assignments");
+            notification.setContentText(message);
+            notification.setWhen(time);
+            notification.setColor(Color.TRANSPARENT);
+            notification.setContentIntent(pendingIntent);
+            notification.addAction(R.mipmap.ic_archive, "View", pendingIntent);
+        }
 
         NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-        notificationManager.notify(0, notificationBuilder.build());
+        notificationManager.notify(0, notification.build());
     }
 }
