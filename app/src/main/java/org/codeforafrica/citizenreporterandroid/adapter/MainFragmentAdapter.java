6a415e8ccc1009f0c49848cd06ce10c20572b631
Jeffkungu <Jeffkungu@gmail.com>
2017-09-26 09:03:15 +0300

    [Feature] add an adapter containing the three main fragments with their titles

---
+++ app/src/main/java/org/codeforafrica/citizenreporterandroid/adapter/MainFragmentAdapter.java
@@ -0,0 +1,38 @@
+package org.codeforafrica.citizenreporterandroid.adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+import java.util.List;
+
+/**
+ * Created by Jeffkungu on 25/09/2017.
+ */
+
+public class MainFragmentAdapter extends FragmentStatePagerAdapter {
+
+    private List<Fragment> fragmentList;
+    private List<String> titleList;
+
+    public MainFragmentAdapter(FragmentManager fm, List<Fragment> fragmentList, List<String> titleList) {
+        super(fm);
+        this.fragmentList = fragmentList;
+        this.titleList = titleList;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return fragmentList.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return fragmentList.size();
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return titleList.get(position);
+    }
+}
