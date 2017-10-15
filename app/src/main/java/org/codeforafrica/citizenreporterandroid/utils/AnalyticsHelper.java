e4ef93f024f2efb265cea71a824bd1a1a6f22d04
Edwin Kato <edwin.kato@andela.com>
2017-10-15 09:17:17 +0300

    Track story saves

--- app/src/main/java/org/codeforafrica/citizenreporterandroid/utils/AnalyticsHelper.java
+++ app/src/main/java/org/codeforafrica/citizenreporterandroid/utils/AnalyticsHelper.java
@@ -19,6 +19,7 @@ public class AnalyticsHelper {
     public static final String EVENT_LOGIN = "LOGIN";
     public static final String EVENT_ASSIGNMENT_OPEN = "ASSIGNMENT_OPEN";
     public static final String EVENT_CREATE_STORY = "CREATE_STORY";
+    public static final String EVENT_SAVE_STORY = "SAVE_STORY";
 
     public static final String PARAM_ASSIGNMENT_TITLE = "assignment_title";
 
