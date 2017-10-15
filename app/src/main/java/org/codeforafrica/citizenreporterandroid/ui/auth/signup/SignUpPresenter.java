a875d887e59855c77001002f5af1da0c7ea0135e
Edwin Kato <edwin.kato@andela.com>
2017-10-15 07:51:32 +0300

    Log password sign-up event

--- app/src/main/java/org/codeforafrica/citizenreporterandroid/ui/auth/signup/SignUpPresenter.java
+++ app/src/main/java/org/codeforafrica/citizenreporterandroid/ui/auth/signup/SignUpPresenter.java
@@ -2,9 +2,15 @@ package org.codeforafrica.citizenreporterandroid.ui.auth.signup;
 
 import android.util.Log;
 import android.widget.Toast;
+
+import com.flurry.android.FlurryAgent;
 import com.parse.ParseException;
 import com.parse.ParseUser;
 import com.parse.SignUpCallback;
+
+import org.codeforafrica.citizenreporterandroid.utils.AnalyticsHelper;
+
+import java.util.HashMap;
 
 /**
  * Created by Ahereza on 9/17/17.
@@ -24,6 +30,7 @@ public class SignUpPresenter implements SignUpContract.Presenter{
     user.signUpInBackground(new SignUpCallback() {
       @Override public void done(ParseException e) {
         if (e == null) {
+          FlurryAgent.logEvent(AnalyticsHelper.EVENT_PASSWORD_SIGN_UP);
           view.showSuccessMessage();
           view.goToSignInActivity();
         } else {
