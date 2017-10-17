8a5ab850bf20d65bfee648e8000059529a1a997a
Seremba John <john.seremba@andela.com>
2017-10-17 16:04:49 +0300

    Display navigation bar on camera

--- app/src/main/java/org/codeforafrica/citizenreporterandroid/camera/CameraActivity.java
+++ app/src/main/java/org/codeforafrica/citizenreporterandroid/camera/CameraActivity.java
@@ -877,9 +877,8 @@ public class CameraActivity extends AppCompatActivity
 			decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
 					| View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
 					| View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-					| View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
 					| View.SYSTEM_UI_FLAG_FULLSCREEN
-					| View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+			);
 		}
 	}
 
