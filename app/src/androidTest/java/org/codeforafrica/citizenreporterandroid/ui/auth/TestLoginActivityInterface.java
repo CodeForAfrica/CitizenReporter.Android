165b20e1ef562e4eb6cd27c6654d2f33d9492d6e
Seremba John <john.seremba@andela.com>
2017-10-18 13:21:49 +0300

    Initialize login activity tests

---
+++ app/src/androidTest/java/org/codeforafrica/citizenreporterandroid/ui/auth/TestLoginActivityInterface.java
@@ -0,0 +1,63 @@
+package org.codeforafrica.citizenreporterandroid.ui.auth;
+
+import android.support.test.filters.LargeTest;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import org.codeforafrica.citizenreporterandroid.R;
+import org.codeforafrica.citizenreporterandroid.ui.auth.login.LoginActivity;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+/**
+ * Created by johnpaulseremba on 18/10/2017.
+ */
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class TestLoginActivityInterface {
+	@Rule
+	public ActivityTestRule<LoginActivity> loginActivityActivityTestRule =
+			new ActivityTestRule<>(LoginActivity.class);
+
+	@Test
+	public void testInterfaceElements() {
+		onView(withId(R.id.facebook_login_button)).check(matches(isDisplayed()));
+		onView(withId(R.id.google_login_button)).check(matches(isDisplayed()));
+		onView(withId(R.id.email_login_button)).check(matches(isDisplayed()));
+		onView(withId(R.id.signin_with_password)).check(matches(isDisplayed()));
+	}
+
+	@Test
+	public void loginWithFacebook() {
+		onView(withId(R.id.facebook_login_button))
+				.check(matches(isDisplayed()))
+				.perform(click());
+
+	}
+
+	@Test
+	public void loginWithGoogle() {
+		onView(withId(R.id.google_login_button))
+				.check(matches(isDisplayed()))
+				.perform(click());
+	}
+
+	@Test
+	public void testSignUp() {
+		onView(withId(R.id.email_login_button))
+				.check(matches(isDisplayed()))
+				.perform(click());
+
+		onView(withText("Sign Up")).check(matches(isDisplayed()));
+	}
+
+
+}
