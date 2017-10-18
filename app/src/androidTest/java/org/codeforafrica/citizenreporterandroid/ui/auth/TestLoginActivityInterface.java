74e2412da978a46aa541a6486063fa681061c55c
Seremba John <john.seremba@andela.com>
2017-10-18 17:54:35 +0300

    test sign up

--- app/src/androidTest/java/org/codeforafrica/citizenreporterandroid/ui/auth/TestLoginActivityInterface.java
+++ app/src/androidTest/java/org/codeforafrica/citizenreporterandroid/ui/auth/TestLoginActivityInterface.java
@@ -1,11 +1,8 @@
 package org.codeforafrica.citizenreporterandroid.ui.auth;
 
-import android.graphics.drawable.Drawable;
 import android.support.test.filters.LargeTest;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
-import android.support.v4.content.ContextCompat;
-import android.widget.ProgressBar;
 import org.codeforafrica.citizenreporterandroid.R;
 import org.codeforafrica.citizenreporterandroid.ui.auth.login.LoginActivity;
 import org.junit.Rule;
@@ -18,10 +15,14 @@ import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.RootMatchers.withDecorView;
+import static android.support.test.espresso.matcher.ViewMatchers.hasErrorText;
+import static android.support.test.espresso.matcher.ViewMatchers.isChecked;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
-import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
 
 /**
  * Created by johnpaulseremba on 18/10/2017.
@@ -62,7 +63,6 @@ public class TestLoginActivityInterface {
 		onView(withId(R.id.email_login_button))
 				.check(matches(isDisplayed()))
 				.perform(click());
-
 		onView(withText("Sign Up")).check(matches(isDisplayed()));
 		onView(withId(R.id.sign_up_name))
 				.check(matches(isDisplayed()))
@@ -73,10 +73,6 @@ public class TestLoginActivityInterface {
 		onView(withId(R.id.sign_up_password))
 				.check(matches(isDisplayed()))
 				.perform(typeText("12345678"), closeSoftKeyboard());
-		onView(withId(R.id.sign_up_done_button))
-				.check(matches(isDisplayed()))
-				.perform(click());
-		onView(withId(R.id.sign_up_progress)).check(matches(isDisplayed()));
 	}
 
 }
