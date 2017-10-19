dd2159ede7ecda514dd79d2aa5b17224e12b75ad
Seremba John <john.seremba@andela.com>
2017-10-19 09:45:29 +0300

    Test Login with facebook

--- app/src/androidTest/java/org/codeforafrica/citizenreporterandroid/ui/auth/TestLoginActivityInterface.java
+++ app/src/androidTest/java/org/codeforafrica/citizenreporterandroid/ui/auth/TestLoginActivityInterface.java
@@ -1,10 +1,16 @@
 package org.codeforafrica.citizenreporterandroid.ui.auth;
 
+import android.support.test.InstrumentationRegistry;
 import android.support.test.filters.LargeTest;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.support.test.uiautomator.UiDevice;
+import android.support.test.uiautomator.UiObject;
+import android.support.test.uiautomator.UiObjectNotFoundException;
+import android.support.test.uiautomator.UiSelector;
 import org.codeforafrica.citizenreporterandroid.R;
 import org.codeforafrica.citizenreporterandroid.ui.auth.login.LoginActivity;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -21,6 +27,7 @@ import static android.support.test.espresso.matcher.ViewMatchers.isChecked;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static junit.framework.TestCase.assertTrue;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.not;
 
@@ -35,6 +42,13 @@ public class TestLoginActivityInterface {
 	public ActivityTestRule<LoginActivity> loginActivityActivityTestRule =
 			new ActivityTestRule<>(LoginActivity.class);
 
+	private UiDevice mDevice;
+
+	@Before
+	public void setUp() throws Exception {
+		mDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
+	}
+
 	@Test
 	public void testInterfaceElements() {
 		onView(withId(R.id.facebook_login_button)).check(matches(isDisplayed()));
@@ -44,15 +58,22 @@ public class TestLoginActivityInterface {
 	}
 
 	@Test
-	public void loginWithFacebook() {
+	public void testLoginWithFacebook() {
 		onView(withId(R.id.facebook_login_button))
 				.check(matches(isDisplayed()))
 				.perform(click());
-
+		UiObject facebookApp = mDevice.findObject(new UiSelector().text("Facebook"));
+		try {
+			facebookApp.clickAndWaitForNewWindow();
+			UiObject fbUserName = mDevice.findObject(new UiSelector().text("username"));
+			assertTrue(fbUserName.exists());
+		} catch (UiObjectNotFoundException e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Test
-	public void loginWithGoogle() {
+	public void testLoginWithGoogle() {
 		onView(withId(R.id.google_login_button))
 				.check(matches(isDisplayed()))
 				.perform(click());
@@ -73,6 +94,11 @@ public class TestLoginActivityInterface {
 		onView(withId(R.id.sign_up_password))
 				.check(matches(isDisplayed()))
 				.perform(typeText("12345678"), closeSoftKeyboard());
+	}
+
+	@Test
+	public void testLoginWithEmail() {
+
 	}
 
 }
