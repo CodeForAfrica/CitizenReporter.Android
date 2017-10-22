cb8848a998943012270b973c384598a28e980f9e
Seremba John <john.seremba@andela.com>
2017-10-22 17:31:26 +0300

    Test visibility of main activity

--- app/src/androidTest/java/org/codeforafrica/citizenreporterandroid/ui/auth/TestLoginWithEmail.java
+++ app/src/androidTest/java/org/codeforafrica/citizenreporterandroid/ui/auth/TestLoginWithEmail.java
@@ -1,10 +1,10 @@
 package org.codeforafrica.citizenreporterandroid.ui.auth;
 
+import android.support.test.espresso.intent.Intents;
+import android.support.test.espresso.intent.rule.IntentsTestRule;
 import android.support.test.filters.LargeTest;
-import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import org.codeforafrica.citizenreporterandroid.R;
-import org.codeforafrica.citizenreporterandroid.ui.auth.login.LoginActivity;
 import org.codeforafrica.citizenreporterandroid.ui.auth.signin.SignInActivity;
 import org.junit.Rule;
 import org.junit.Test;
@@ -17,6 +17,8 @@ import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
 
 /**
  * Created by johnpaulseremba on 22/10/2017.
@@ -26,8 +28,8 @@ import static android.support.test.espresso.matcher.ViewMatchers.withId;
 @RunWith(AndroidJUnit4.class)
 public class TestLoginWithEmail {
 	@Rule
-	public ActivityTestRule<SignInActivity> loginActivityActivityTestRule =
-			new ActivityTestRule<>(SignInActivity.class);
+	public IntentsTestRule<SignInActivity> loginWithEmailTestRule =
+			new IntentsTestRule<SignInActivity>(SignInActivity.class);
 
 	@Test
 	public void testLoginWithEmail() {
@@ -40,5 +42,6 @@ public class TestLoginWithEmail {
 		onView(withId(R.id.sign_in_button_done))
 				.check(matches(isDisplayed()))
 				.perform(click());
+		onView(withText("Citizen Reporter")).check(matches(isDisplayed()));
 	}
 }
